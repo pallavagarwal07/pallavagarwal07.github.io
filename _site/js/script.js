67a486d46d00870ece048db7e8bb8cc237bf0b5a
Pallav Agarwal <pallavag@iitk.ac.in>
2015-06-18 22:08:59 +0530

    Setup folder structure

---
+++ _site/js/script.js
@@ -0,0 +1,28 @@
+(function() {
+  var parallax;
+
+  window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function(f) {
+    return setTimeout(f, 1000 / 60);
+  };
+
+  parallax = function() {
+    var banH, divH, img, imgH, imgW, scrollTop;
+    img = $('#bg-img');
+    imgH = parseInt(img.css('height'));
+    imgW = parseInt(img.css('width'));
+    banH = parseInt($('#menu').css('height'));
+    divH = parseInt($('#image').css('height'));
+    if ((banH + divH) > imgH) {
+      $('#image').css('height', (imgH - banH) + 'px');
+    }
+    scrollTop = window.pageYOffset;
+    return $('#bg-img').css('top', scrollTop * 1.3 + 'px');
+  };
+
+  window.addEventListener('scroll', (function() {
+    return requestAnimationFrame(parallax);
+  }), false);
+
+  parallax();
+
+}).call(this);
